130e7a7a7d5f5b060c8385ba8ef41ac5e12f8f1c
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:42:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1;
+            Double n2;
+            Double soma;
+            Double area;
+
+            Console.WriteLine("Coloque o valor da base(b) do triangulo");
+            n1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque o valor da altura(h) do triangulo");
+            n2 = Double.Parse(Console.ReadLine());
+            soma = n1 * n2;
+            area = soma / 2;
+            Console.WriteLine("O valor da Área do triangulo");
+            Console.WriteLine(area);
+        }
+    }
+}
